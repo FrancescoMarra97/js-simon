6b71e0ac92bb692b2c2f037a2dfdc2c91012b84f
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-10 14:57:15 +0200

    funzione genera numero

---
+++ main.js
@@ -0,0 +1,18 @@
+/* 
+- Visualizzare in pagina 5 numeri casuali.
+- Da lì parte un timer di 30 secondi.
+- Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente,
+  nell'ordine che preferisce.
+- Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+ */
+
+// fase 1 creazione di 5 numeri casuali
+function generaNum() {
+    let numbers=[];
+    for (let i = 0; i < 5; i++) {
+   //genero un numero casuale in questo caso tra 1 e 100
+    let numeroGenerato = Math.floor(Math.random() * (100 - 1) ) + 1;
+    
+    }
+
+}
